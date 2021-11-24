7f51aadf1096d5d585db24e4c663954a5b9c5295
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2021-11-23 21:02:53 -0800

    new commit

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -29,3 +29,24 @@ myFunction(x)
 x.addListener(myFunction)
 
 })
+
+let drops = document.querySelectorAll('.drops_content')
+let drops_items = document.querySelectorAll('.drops')
+let num2 = 0;
+drops.forEach((e)=>{
+	e.onclick = function(){
+		num2++
+		let icons = document.querySelectorAll('.down_icons')
+		let index_drops = Array.prototype.indexOf.call(drops, e)
+
+		if(num2 == 2){num2 = 0}
+		if (num2 == 1) {
+		drops_items[index_drops].style = " height:auto; transition:all 0.3s;"
+		icons[index_drops].style = "transform:rotate(180deg); transition:all 0.3s;"
+	}else{
+		drops_items[index_drops].style = "height:0px; transition:all 0.3s;"
+		icons[index_drops].style = "transform:rotate(0deg); transition:all 0.3s;"
+	}
+		
+	}
+})
